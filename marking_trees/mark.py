0f4f4340e4780175abdc664e36fbcc4f7ec39760
wribbe <eng_steff@hotmail.com>
2017-08-31 16:00:00 +0200

    Re-written variant handles example tree.

--- marking_trees/mark.py
+++ marking_trees/mark.py
@@ -6,123 +6,86 @@ import random
 MARKED = 'm'
 UNMARKED = 'u'
 
-R2_indexes = []
-R3_indexes = []
+example_tree = [MARKED, UNMARKED, UNMARKED, MARKED, UNMARKED, MARKED, UNMARKED]
 
-num_marked = 0
-global_N = 0
+def sibling(index):
+    if index%2 == 0:
+        return index-1
+    return index+1
 
-def main():
-    start_h = 2
-    max_h = 20
+def parent(index):
+    half_index = int(index/2)
+    if index%2 == 0:
+        return half_index-1
+    return half_index
 
-    def new_tree(N):
-        global global_N
-        global num_marked
-        global_N = N
-        num_marked = 0
-        return ([False]*N, [UNMARKED]*N)
+def children(index):
+    left_child = (index*2)+1
+    right_child = left_child+1
+    return [left_child, right_child]
 
-    def even(value):
-        return value%2 == 0
+def mark(index, tree):
+    tree[index] = MARKED
 
-    def parent(index):
-        if even(index):
-            return int((index/2)-1)
-        return int(index/2)
+def marked(index, tree):
+    if type(index) == list:
+        return all([marked(ind, tree) for ind in index])
+    if index > len(tree)-1 or index < 0:
+        return False
+    return tree[index] == MARKED
 
-    def sibling(index):
-        if even(index):
-            return index-1
-        return index+1
+def marking_rule(index, tree):
 
-    def children(index):
-        first = index*2+1
-        second = first+1
-        return [first, second]
+    # 1. An internal node gets marked when both its children are marked.
+    # 2. A non-root node gets marked if its parent and sibling are marked.
+    if marked(children(index), tree):
+        return True
+    elif marked([parent(index), sibling(index)], tree):
+        return True
 
-    def mark1(index):
-        mark(index-1)
+def R1(N):
+    return random.randint(1,N-1)
 
-    def mark(index):
-        global num_marked
-        if marked(index):
-            return
-        tree[index] = MARKED;
+def R2(N, stored):
+    return stored.pop()
+
+def R3(N, stored, tree):
+    index = stored.pop()
+    while tree[index]:
+        index = stored.pop()
+    return index
+
+def traverse_marking_rule(index, tree, visited):
+    if index < 0 or index > len(tree)-1 or visited[index]:
+        return 0
+    num_marked = 0
+    visited[index] = True
+    if not marked(index, tree) and marking_rule(index, tree):
+        mark(index, tree)
         num_marked += 1
 
-    def marked(index):
-        if type(index) == list:
-            return all([marked(ind) for ind in index])
-        if index > len(tree)-1:
-            return False
-        return tree[index] == MARKED
+    left, right = children(index)
+    num_marked += traverse_marking_rule(left, tree, visited)
+    num_marked += traverse_marking_rule(right, tree, visited)
 
-    def execute_rule(index):
-        if type(index) == list:
-            for ind in index:
-                execute_rule(ind)
+    num_marked += traverse_marking_rule(parent(index), tree, visited)
+    return num_marked
+
+def mark_tree(tree, function, function_input, predefined_indexes=[]):
+
+    N = len(tree)
+    num_marked = len([val for val in tree if val == MARKED])
+    rotations = 0
+    while num_marked != N:
+        if predefined_indexes:
+            new_index = predefined_indexes.pop()
         else:
-            if index < 0 or index > global_N-1 or visited[index]:
-                return
-            if not marked(index):
-                if marked(children(index)) or \
-                   marked([parent(index), sibling(index)]):
-                    print("executed rule..")
-                    mark(index)
-            visited[index] = True
-            execute_rule(parent(index)) # Continue upwards.
-            execute_rule(children(index)) # Continue downwards.
-
-    def all_marked():
-        return num_marked == global_N
-
-    def R1(N):
-        index = random.randint(0,N-1)
-        mark(index)
-        return index
-
-    def R2(N):
-        global R2_indexes
-        if not R2_indexes:
-            R2_indexes = list(range(N))
-            random.shuffle(R2_indexes)
-        index = R2_indexes.pop()
-        mark(index)
-        return index
-
-    def R3(N):
-        global R3_indexes
-        if not R3_indexes:
-            R3_indexes = list(range(N))
-            random.shuffle(R3_indexes)
-        index = R3_indexes.pop()
-        while marked(index): # Throw away already marked indexes.
-            index = R3_indexes.pop()
-        mark(index)
-        return index
-
-    def run_untill_marked(N, marking_function):
-        global tree
-        global visited
-        visited, tree = new_tree(N)
-        rounds = 0
-        while not all_marked():
-            index = marking_function(N)
-            vistited = [False] * global_N
-            execute_rule(index)
-            rounds += 1
-        return rounds
-
-    for h in range(start_h, max_h+1):
-
-        N = int(math.pow(2, h)-1)
-
-        rounds = [run_untill_marked(N, method) for method in [R1,R2,R3]]
-
-        print("N={}, h={}:".format(N, h))
-        for index, num_rounds in enumerate(rounds, start=1):
-            print("  R{} = {}".format(index, num_rounds))
-
-if __name__ == "__main__":
-    main()
+            new_index = function(*function_input)
+        if not marked(new_index, tree):
+            mark(new_index, tree)
+            num_marked += 1
+            num_marked += traverse_marking_rule(new_index, tree, [False] * N)
+        rotations += 1
+    return rotations
+rotations = mark_tree(example_tree, R1, [len(example_tree)], [4])
+print(rotations)
