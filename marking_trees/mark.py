2ad43fe7fb5f5d3c1b74b3a0e1a7fb4ce89ba469
AdamOhlsson <zba10aoh@student.lu.se>
2017-09-03 18:41:25 +0200

    Queue affected nodes for marking, turning the recursive marking algorithm into iteration over a bounded but expanding queue.

--- marking_trees/mark.py
+++ marking_trees/mark.py
@@ -6,8 +6,6 @@ import random
 MARKED = 'm'
 UNMARKED = 'u'
 
-example_tree = [MARKED, UNMARKED, UNMARKED, MARKED, UNMARKED, MARKED, UNMARKED]
-
 def sibling(index):
     if index%2 == 0:
         return index-1
@@ -24,24 +22,39 @@ def children(index):
     right_child = left_child+1
     return [left_child, right_child]
 
-def mark(index, tree):
-    tree[index] = MARKED
+def has_children(index, tree):
+    return [i for i in children(index) if i < len(tree)] != []
 
 def marked(index, tree):
-    if type(index) == list:
-        return all([marked(ind, tree) for ind in index])
-    if index > len(tree)-1 or index < 0:
-        return False
-    return tree[index] == MARKED
+    return tree[index][0] == MARKED
 
-def marking_rule(index, tree):
+def mark(index, tree):
+    # Collect and return cascade markings that should be made due to marking
+    # index.
+    cascade = []
+    tree[index] = (MARKED, tree[index][1])
+    if has_children(index, tree):
+        child = children(index)
+        if marked(child[0], tree) and not marked(child[1], tree):
+            cascade += [child[1]]
+        elif marked(child[1], tree) and not marked(child[0], tree):
+            cascade += [child[0]]
 
-    # 1. An internal node gets marked when both its children are marked.
-    # 2. A non-root node gets marked if its parent and sibling are marked.
-    if marked(children(index), tree):
-        return True
-    elif marked([parent(index), sibling(index)], tree):
-        return True
+    if index > 0:
+        if marked(sibling(index), tree) and not marked(parent(index), tree):
+            cascade += [parent(index)]
+        elif marked(parent(index), tree) and not marked(sibling(index), tree):
+            cascade += [sibling(index)]
+    return cascade
+
+def mark_tree(value, tree):
+    mark_queue = [value] if not marked(value, tree) else []
+    n_marked = 0
+    while mark_queue:
+        n_marked += len(mark_queue)
+        sub_queues = [mark(i, tree) for i in mark_queue if not marked(i, tree)]
+        mark_queue = [index for sub_queue in sub_queues for index in sub_queue]
+    return n_marked
 
 def R1(N):
     return random.randint(0,N-1)
@@ -49,89 +62,64 @@ def R1(N):
 def R2(stored):
     return stored.pop()
 
-def R3(stored, tree):
-    index = stored.pop()
-    while marked(index, tree):
+def R3(tree):
+    def lam(stored):
         index = stored.pop()
-    return index
+        while marked(index, tree):
+            index = stored.pop()
+        return index
+    return lam
 
-def iterative_marking_rule(index, tree, visited, initial_state=None):
-    if index < 0 or index > len(tree)-1 or visited[index]:
-        return 0
-    num_marked = 0
-    visited[index] = True
-    if not initial_state:
-        initial_state = tree[index]
-    if not marked(index, tree) and marking_rule(index, tree):
-        mark(index, tree)
-        num_marked += 1
-
-    # No need to traverse upwards, downwards or signal sibling if there was no
-    # state change.
-    if (initial_state != tree[index]):
-        num_marked += iterative_marking_rule(parent(index), tree, visited)
-        num_marked += iterative_marking_rule(sibling(index), tree, visited)
-        if not marked(children(index), tree):
-            left, right = children(index)
-            num_marked += iterative_marking_rule(left, tree, visited)
-            num_marked += iterative_marking_rule(right, tree, visited)
-    return num_marked
-
-def mark_tree(tree, function, function_input, input_indexes=[]):
-
-    N = len(tree)
-    num_marked = len([val for val in tree if val == MARKED])
+def run(index_generator, seed, tree):
     iterations = 0
-    while num_marked != N:
-        if input_indexes:
-            new_index = input_indexes.pop()
-        else:
-            new_index = function(*function_input)
-        if not marked(new_index, tree):
-            num_marked += 1
-            mark(new_index, tree)
-            num_marked += iterative_marking_rule(new_index, tree, [False] * N,
-                    UNMARKED)
+    n_marked = sum([1 for i in range(0, len(tree)) if marked(i, tree)])
+    while n_marked != len(tree):
         iterations += 1
-        dynamic_status = "INFO: Current iteration: {}\r".format(iterations)
-        print(dynamic_status, end="")
-    # Clear the current iteration print.
-    print("{}\r".format(" "*len(dynamic_status)), end="")
+        n_marked += mark_tree(index_generator(seed), tree)
     return iterations
 
 def main():
-
-    # Example tree from paper, should complete in one round.
-    iterations = mark_tree(example_tree, R1, [len(example_tree)], [4])
-    if not iterations == 1:
-        print("Example tree should complete in one iteration " +\
-              "with R1 and input-index 4, aborting. ")
-        return
-
     h_max = 20
     h_start = 2
     for h in range(h_start,h_max+1):
         N = int(math.pow(2,h)-1)
         print("h={},N={}".format(h,N))
         # Setup for R1.
-        tree = [UNMARKED] * N
-        R1_iterations = mark_tree(tree, R1, [N])
+        tree = [(UNMARKED, i) for i in range(0, N)]
+        R1_iterations = run(R1, N, tree)
         print("  R1 - iterations: {}".format(R1_iterations))
 
         # Setup for R2.
-        tree = [UNMARKED] * N
+        tree = [(UNMARKED, i) for i in range(0, N)]
         stored = list(range(N))
         random.shuffle(stored)
-        R2_iterations = mark_tree(tree, R2, [stored])
+        R2_iterations = run(R2, stored, tree)
         print("  R2 - iterations: {}".format(R2_iterations))
 
         # Setup for R3.
-        tree = [UNMARKED] * N
+        tree = [(UNMARKED, i) for i in range(0, N)]
         stored = list(range(N))
         random.shuffle(stored)
-        R3_iterations = mark_tree(tree, R3, [stored, tree])
+        R3_iterations = run(R3(tree), stored, tree)
         print("  R3 - iterations: {}".format(R3_iterations))
         print()
 
+def test_example():
+    example_tree = [
+        (MARKED, 0),(UNMARKED, 1),(UNMARKED, 2),(MARKED, 3),(UNMARKED, 4),
+        (MARKED, 5),(UNMARKED, 6)
+    ]
+
+    #Example tree from paper, should complete in one round.
+    iterations = run(lambda seed: seed, 4, example_tree)
+    if not iterations == 1:
+        print("Example tree should complete in one iteration " +\
+              "with R1 and input-index 4, aborting. ")
+        return False
+    return True
+
 if __name__ == "__main__":
-    main()
+    if test_example():
+        main()
+    else:
+        print("Test example failed. Abort.")
