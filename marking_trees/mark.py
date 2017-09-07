ef4ff79cc96e1355e015ecb6fbae0c3ca0e42a4b
wribbe <eng_steff@hotmail.com>
2017-09-07 12:22:03 +0200

    Refactor how printing is done to make it possible to dump data.

--- marking_trees/mark.py
+++ marking_trees/mark.py
@@ -6,7 +6,9 @@ import sys
 
 MARKED = 'm'
 UNMARKED = 'u'
+
 INFO_OUTPUT = True
+OUTPUT_JSON = False
 
 def sibling(index):
     if index%2 == 0:
@@ -125,30 +127,100 @@ def run(index_generator, generator_input, tree):
     return iterations
 
 def main():
+
     h_max = 20
     h_start = 2
+    data = []
+
+    KEY_HEADING = 'heading'
+    KEY_ITERATIONS = 'iterations'
+
+    KEY_R1 = "R1"
+    KEY_R2 = "R2"
+    KEY_R3 = "R3"
+
+    def add_to_dict(dictionary, keys, value, last=False):
+
+        output_formats = {
+            KEY_HEADING : "h={}, N={}",
+            KEY_ITERATIONS: "{} - iterations: {}",
+        }
+
+        def output_printer(fmt_text, values, last):
+            """ Print to standard out or standard error depending on JSON
+            output.
+            """
+
+            # Ensure that values are wrapped in a list to enable expansion.
+            if not type(values) == list:
+                values = [values]
+
+            output_stream = sys.stdout
+            if OUTPUT_JSON:
+                output_stream = sys.stderr
+            print(fmt_text.format(*values), file=output_stream)
+            if last: # Additional spacing after last entry.
+                print()
+
+        # Make sure that it's possible to iterate over provided keys.
+        if not type(keys) == list:
+            keys = [keys]
+
+        def make_sure_dict_exists(dictionary, key):
+            """ Make sure that there is a dictionary corresponding to the given
+            key, if there is none, create it.
+            """
+            exists = dictionary.get(key)
+            if not exists:
+                exists = dictionary[key] = {}
+            return exists
+
+        # Retrieve main dictionary key, and corresponding data.
+        main_key = keys[0]
+        make_sure_dict_exists(dictionary, main_key)
+
+        # The final value should be at current_dict[last_key].
+        current_dict = dictionary
+        last_key = main_key
+        if keys:
+            last_key = keys.pop(-1)
+        for key in keys: # Build rest of intermediate dictionary structure.
+            current_dict = make_sure_dict_exists(current_dict, key)
+
+        # At bottom of existing or created dictionary structure, add value.
+        current_dict[last_key] = value
+        # Print output with predefined formatting.
+        output_printer(output_formats[main_key], value, last)
+
     for h in range(h_start,h_max+1):
+
+        # Create and append data dictionary.
+        dict_data = {}
+        data.append(dict_data)
+
+        # Setup and add heading to dict.
         N = int(math.pow(2,h)-1)
-        print("h={},N={}".format(h,N))
+        add_to_dict(dict_data, KEY_HEADING, [h, N])
+
         # Setup for R1.
         tree = [UNMARKED] * N
         R1_iterations = run(R1, [N], tree)
-        print("  R1 - iterations: {}".format(R1_iterations))
+        add_to_dict(dict_data, [KEY_ITERATIONS, KEY_R1], ["R1", R1_iterations])
 
         # Setup for R2.
         tree = [UNMARKED] * N
         stored = list(range(N))
         random.shuffle(stored)
         R2_iterations = run(R2, [stored], tree)
-        print("  R2 - iterations: {}".format(R2_iterations))
+        add_to_dict(dict_data, [KEY_ITERATIONS, KEY_R2], ["R2", R2_iterations])
 
         # Setup for R3.
         tree = [UNMARKED] * N
         stored = list(range(N))
         random.shuffle(stored)
         R3_iterations = run(R3, [stored, tree], tree)
-        print("  R3 - iterations: {}".format(R3_iterations))
-        print()
+        add_to_dict(dict_data, [KEY_ITERATIONS, KEY_R3], ["R3", R3_iterations],
+                last=True)
 
 def test_tree_passes():
     # Example tree from paper, should complete in one round.
@@ -168,6 +240,8 @@ if __name__ == "__main__":
     args = sys.argv[1:]
     if '--no-info' in args:
         INFO_OUTPUT = False
+    if '--json' in args:
+        OUTPUT_JSON = True
 
     # Test if test tree passes, otherwise abort.
     if not test_tree_passes():
