9ed070888474550944884153a7196a9a9e3cfefe
wribbe <eng_steff@hotmail.com>
2017-09-11 14:34:32 +0200

    Change flag --print-tree -> --print-data.

--- maxcut/maxcut_r.py
+++ maxcut/maxcut_r.py
@@ -4,7 +4,7 @@ import sys
 
 EXIT_ERROR = -1
 
-def print_graph(vertices):
+def print_data(vertices):
     # {x} specifies the x'th input to be used by format.
     # "{0}-{0}-{1}".format('a','b') -> "a-a-b".
     format_vertex = "vertex={0} edge={0}--{1}, w={2}"
@@ -16,7 +16,7 @@ def print_graph(vertices):
 
 def main(args):
 
-    usage = "[python] {} input_file.txt [--print-tree]"
+    usage = "[python] {} input_file.txt [--print-data]"
     if not args:
         print(usage.format(__file__))
         return EXIT_ERROR
@@ -50,8 +50,8 @@ def main(args):
               file=sys.stderr)
         return EXIT_ERROR
 
-    if '--print-tree' in args:
-        print_graph(vertices)
+    if '--print-data' in args:
+        print_data(vertices)
 
 if __name__ == "__main__":
     args = sys.argv[1:]
